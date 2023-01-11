2add4cd874ed55359c1836b6f05b79a019dc20b9
Даниил Кочура <kochura2017@yandex.ru>
2023-01-11 12:45:44 +0300

    Добавил движок smarty и поправил верстку

---
+++ views/inc/footer.php
@@ -0,0 +1,11 @@
+
+<!-- Core javascripts -->
+
+<script src="assets/js/core.min.js"></script>
+<script src="assets/js/vendor.datatables.js"></script>
+
+
+<!-- Your custom javascripts -->
+
+</body>
+</html>
---
+++ views/inc/header.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<!-- <html lang="ar" dir="rtl"> -->
+<html lang="en" dir="ltr">
+<head>
+
+	<title>Page title</title>
+	<meta name="description" content="...">
+
+	<meta charset="utf-8">
+	<meta name="MobileOptimized" content="320"/>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+	<!-- speed up external res -->
+	<link rel="dns-prefetch" href="https://fonts.googleapis.com/">
+	<link rel="dns-prefetch" href="https://fonts.gstatic.com/">
+	<link rel="preconnect" href="https://fonts.googleapis.com/">
+	<link rel="preconnect" href="https://fonts.gstatic.com/">
+	<!-- preloading icon font is helping to speed up a little bit -->
+	<link rel="preload" href="assets/fonts/flaticon/Flaticon.woff2" as="font" type="font/woff2" crossorigin>
+	<link rel="stylesheet" href="assets/css/vendor.datatables.css">
+	<link rel="stylesheet" href="assets/css/core.min.css">
+	<link rel="stylesheet" href="assets/css/vendor_bundle.min.css">
+	<link rel="stylesheet" href="assets/css/custom.css">
+	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700&display=swap" rel="stylesheet">
+
+</head>
+<body>
